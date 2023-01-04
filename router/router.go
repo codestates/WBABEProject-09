6b97d1e2d4ecda65beabc061937c05d17512a5f1
ykkim <code.sugartoast@gmail.com>
2023-01-04 16:55:37 +0900

    피드백 반영: 변경에 따른 실시간 endpoint 관리

--- router/router.go
+++ router/router.go
@@ -77,31 +77,37 @@ func (p *Router) Idx() *gin.Engine {
 	e.POST("/user", p.ct.InsertUserControl) // 유저 등록 주소로 처음 환경 초기화시 사용을 위해 추가
 	e.POST("/test", p.ct.TestControl)       // 일부 작은 단위의 기능을 테스트하기 위한 주소로 swagger에 등록하지 않음
 
-	owner := e.Group("owner", liteAuth())
+	// 실시간으로 서버가 동작하는 환경에서 기능 변경을 고려해 version 그룹을 추가
+	// 기존 요청은 v1로 유지하면서 신규 요청은 v2로 전환 가능
+	version1 := e.Group("v1")
 	{
-		owner.GET("/menu", p.ct.GetMenuControl) // 임시로 GetOk로 연결
-		owner.GET("/menu/detail", p.ct.GetMenuDetailControl)
-		owner.POST("/menu", p.ct.InsertMenuControl)
-		owner.PUT("/menu", p.ct.UpdateMenuControl)
-		owner.DELETE("/menu", p.ct.DeleteMenuControl)
+		owner := version1.Group("owner", liteAuth())
+		{
+			owner.GET("/menu", p.ct.GetMenuControl) // 임시로 GetOk로 연결
+			owner.GET("/menu/detail", p.ct.GetMenuDetailControl)
+			owner.POST("/menu", p.ct.InsertMenuControl)
+			owner.PUT("/menu", p.ct.UpdateMenuControl)
+			owner.DELETE("/menu", p.ct.DeleteMenuControl)
 
-		owner.GET("/order", p.ct.GetOrderControl)         // 오더 상태 확인
-		owner.PUT("/order", p.ct.UpdateOwnerOrderControl) // 오더 상태 수정
-	}
+			owner.GET("/order", p.ct.GetOrderControl)         // 오더 상태 확인
+			owner.PUT("/order", p.ct.UpdateOwnerOrderControl) // 오더 상태 수정
+		}
 
-	customer := e.Group("customer", liteAuth())
-	{
-		customer.GET("/menu", p.ct.GetMenuControl)
-		customer.GET("/menu/detail", p.ct.GetMenuDetailControl) // 개별 메뉴에 대한 평점 및 리뷰 데이터 확인
+		customer := version1.Group("customer", liteAuth())
+		{
+			customer.GET("/menu", p.ct.GetMenuControl)
+			customer.GET("/menu/detail", p.ct.GetMenuDetailControl) // 개별 메뉴에 대한 평점 및 리뷰 데이터 확인
 
-		customer.GET("/order", p.ct.GetOrderControl)             // 자신이 주문한 order 확인
-		customer.POST("/order", p.ct.InsertCustomerOrderControl) // order 주문
-		customer.PUT("/order", p.ct.UpdateCustomerOrderControl)  // 자신이 주문한 order 정보 변경, 삭제 대신 취소 상태로 대신함
+			customer.GET("/order", p.ct.GetOrderControl)             // 자신이 주문한 order 확인
+			customer.POST("/order", p.ct.InsertCustomerOrderControl) // order 주문
+			customer.PUT("/order", p.ct.UpdateCustomerOrderControl)  // 자신이 주문한 order 정보 변경, 삭제 대신 취소 상태로 대신함
 
-		customer.GET("/order/review", p.ct.GetReviewControl)       // 자신이 주문한 order에 대한 리뷰 확인
-		customer.POST("/order/review", p.ct.InsertReviewControl)   // 자신이 주문한 order에 대한 리뷰 추가
-		customer.PUT("/order/review", p.ct.UpdateReviewControl)    // 자신이 주문한 order에 대한 리뷰 수정
-		customer.DELETE("/order/review", p.ct.DeleteReviewControl) // 자신이 주문한 order에 대한 리뷰 삭제
+			customer.GET("/order/review", p.ct.GetReviewControl)       // 자신이 주문한 order에 대한 리뷰 확인
+			customer.POST("/order/review", p.ct.InsertReviewControl)   // 자신이 주문한 order에 대한 리뷰 추가
+			customer.PUT("/order/review", p.ct.UpdateReviewControl)    // 자신이 주문한 order에 대한 리뷰 수정
+			customer.DELETE("/order/review", p.ct.DeleteReviewControl) // 자신이 주문한 order에 대한 리뷰 삭제
+		}
+
 	}
 
 	return e
